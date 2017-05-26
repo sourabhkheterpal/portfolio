d7f05279346e62f8f1c8209a33d30b163f419d97
Sourabh Kheterpal <sourabhk@paymango.com>
2017-05-26 11:53:35 +0530

    Portfolios new/create

--- app/controllers/portfols_controller.rb
+++ app/controllers/portfols_controller.rb
@@ -4,4 +4,20 @@ class PortfolsController < ApplicationController
     @portfolio_items = Portfol.all
   end
 
+  def new
+    @portfolio_item = Portfol.new
+  end
+
+  def create
+    @portfolio_item = Portfol.new(params.require(:portfol).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfols_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
